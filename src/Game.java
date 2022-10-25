7e46cd482293630a627fa873c6d2dcc57e397c80
trox18 <95303754+trox18@users.noreply.github.com>
2022-10-25 20:06:23 +0200

    Game class first implementation

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,99 @@
+import game.Board;
+import game.Computer;
+import game.Player;
+import game.User;
+
+import java.util.Random;
+
 public class Game {
 
-    public Game(){
+    // Prototype Player
+    Player player1;
+    Player player2;
+    // Prototype Boards
+    Board boardUser;
+    Board boardComp;
+    Board boardTarget;
 
-        System.out.println("Hello World");
+    public Game() {
+
+        System.out.println("Starting new game...");
+
+        // create board & players
+        setupGame();
+
+        // set initial boat positions
+        player1.initBoard();
+        player2.initBoard();
+
+        // Gameloop
+        do {
+            fight();
+        } while (~player1.fleet.isEmpty() && ~player2.fleet.isEmpty()); // TODO: Check whether fleet of one player is destroyed
+
+        // Game Over
+        gameOver();
+
 
     }
+
+    private void setupGame() {
+        // create board
+        boardUser = new Board();
+        boardComp = new Board();
+        boardTarget = new Board();
+
+        // set user as either player 1 or 2
+        // create random boolean
+        Random rand = new Random();
+        final boolean r = rand.nextBoolean();
+
+        if (r) {
+            // if True: Player1 -> User; Player2 -> Computer
+            player1 = new User(boardUser, boardTarget);
+            player2 = new Computer(boardComp);
+        } else {
+            // else: Player1 -> Computer; Player2 -> User
+            player1 = new Computer(boardComp);
+            player2 = new User(boardUser, boardTarget);
+        }
+
+    }
+
+    private void fight() {
+
+        player1.attack();
+        player2.attack();
+
+        // print Gameboard
+        printBoard();
+
+    }
+
+    private void gameOver(){
+        Player winner;
+        if(player1.fleet.isEmpty()){    // TODO: Check if fleet of player1 is destroyed
+            winner=player2;
+        }else {
+            winner=player1;
+        }
+
+        // Game Over screen
+        System.out.println("################################################");
+        System.out.println("################################################");
+
+        // Winner message
+        if(winner instanceof User){
+            System.out.println("#############      You won!        #############");
+        } else {
+            System.out.println("#############      You lost!       #############");
+        }
+        System.out.println("################################################");
+        System.out.println("################################################");
+        System.out.println("#############  Thanks for playing  #############");
+        System.out.println("################################################");
+
+    }
+
 
 }
