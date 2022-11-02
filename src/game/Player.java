1ec9bc6d3cdfd367295c58c2dd830e890c4b198b
Kavishan Srirangarasa <kavishanwin8@hotmail.com>
2022-11-02 10:53:27 +0100

    added validatePos()

--- src/game/Player.java
+++ src/game/Player.java
@@ -2,8 +2,8 @@ package game;
 
 public class Player {
     public Fleet fleet = new Fleet();
-    public Board aBoardUser = new Board();
-    public Board aBoardComp = new Board();
+    public Board aBoardUser;
+    public Board aBoardComp;
 
     int posX, posY;
     // set initial positions of boats on board
@@ -29,8 +29,33 @@ public class Player {
     }
 
 
-    public void validateMove(char x, int y) {
+    public boolean validatePos(int align, int pos, int size) {
+        //check if out of board
+        if (align == 1) {
+            if (!(pos / 10 + size <= 9)) {
+                return false;
+            }
+        }
+        else {
+            if (!(pos % 10 + size <= 9)) {
+                return false;
+            }
+        }
 
+        String[][] grid = aBoardUser.getGrid();
+
+        //check collision
+        for (int i = 0; i < size; i++) {
+
+            if (align == 1) {
+                if (!(grid[pos / 10 + i][pos % 10] == null)){
+                    return false;}
+            } else {
+                if (!(grid[pos / 10][pos % 10 + i] == null)){
+                    return false;}
+            }
+        }
+        return true;
     }
 
 }
