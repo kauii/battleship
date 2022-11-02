afb5c79cde1624a664684afcbdbf356ae484c185
Kavishan Srirangarasa <kavishanwin8@hotmail.com>
2022-11-02 13:04:24 +0100

    corrections on Player, Computer & User

--- src/game/Computer.java
+++ src/game/Computer.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Computer extends Player{
     Random r = new Random();
+    Board aBoardComp;
 
-    public Computer(Board pBoardUser, Board pBoardComputer){
+    public Computer(Board pBoardComputer){
         aBoardComp = pBoardComputer;
-        aBoardUser = pBoardUser;
     }
 
     @Override
@@ -22,6 +22,7 @@ public class Computer extends Player{
 
     @Override
     public void initBoard() throws Exception {
+        System.out.println("Computer is deploying ships..");
         int align;
         int pos;
         int[] position;
@@ -29,8 +30,9 @@ public class Computer extends Player{
             for (int j = 0; j <= i; j++) {
                 do {
                     System.out.println(fleet.boats[i][j].getType());
-                    align = r.nextInt(0,1);
-                    pos = r.nextInt(0,99);
+                    align = r.nextInt(0,2);
+                    System.out.println("align is: " + align); // TODO:remove later for Computer
+                    pos = calculatePos(align, fleet.boats[i][j].getSize());
                 } while (!validatePos(align, pos,fleet.boats[i][j].getSize()));
                 position = fleet.boats[i][j].setPosition(align,pos);
                 aBoardComp.setGrid(fleet.boats[i][j].getType(), position);
@@ -39,7 +41,52 @@ public class Computer extends Player{
 
     }
 
+    private int calculatePos(int align, int size ) {
+        if (align == 0) {
+            posX = r.nextInt(0,10);
+            posY = r.nextInt(0, 11- size);
+        } else {
+            posX = r.nextInt(0,11 - size);
+            posY = r.nextInt(0,10);
+        }
+        System.out.println(posX + "," + posY); //TODO:remove later for Computer
+        return posX * 10 + posY;
+    }
+
     public void calculateMove() {
+    }
+
+    private boolean validatePos(int align, int pos, int size) {
+        //check if out of board
+        if (align == 1) {
+            if (!(pos / 10 + size <= 10)) {
+                System.out.println("Invalid input. Ship is outside of the grid.");   // TODO:remove later for Computer
+                return false;
+            }
+        }
+        else {
+            if (!(pos % 10 + size <= 10)) {
+                System.out.println("Invalid input. Ship is outside of the grid."); // TODO:remove later for Computer
+                return false;
+            }
+        }
+
+        String[][] grid = aBoardComp.getGrid();
+
+        //check collision
+        for (int i = 0; i < size; i++) {
+
+            if (align == 1) {
+                if (!(grid[pos / 10 + i][pos % 10] == null)){
+                    System.out.println("Invalid input. Another ship has already been placed on this position."); // TODO:remove later for Computer
+                    return false;}
+            } else {
+                if (!(grid[pos / 10][pos % 10 + i] == null)){
+                    System.out.println("Invalid input. Another ship has already been placed on this position."); // TODO:remove later for Computer
+                    return false;}
+            }
+        }
+        return true;
     }
 
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -2,8 +2,6 @@ package game;
 
 public class Player {
     public Fleet fleet = new Fleet();
-    public Board aBoardUser;
-    public Board aBoardComp;
 
     int posX, posY;
     // set initial positions of boats on board
@@ -29,33 +27,6 @@ public class Player {
     }
 
 
-    public boolean validatePos(int align, int pos, int size) {
-        //check if out of board
-        if (align == 1) {
-            if (!(pos / 10 + size <= 9)) {
-                return false;
-            }
-        }
-        else {
-            if (!(pos % 10 + size <= 9)) {
-                return false;
-            }
-        }
 
-        String[][] grid = aBoardUser.getGrid();
-
-        //check collision
-        for (int i = 0; i < size; i++) {
-
-            if (align == 1) {
-                if (!(grid[pos / 10 + i][pos % 10] == null)){
-                    return false;}
-            } else {
-                if (!(grid[pos / 10][pos % 10 + i] == null)){
-                    return false;}
-            }
-        }
-        return true;
-    }
 
 }
