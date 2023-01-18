c55becfc68061c726c338640dea8ea184728c02d
Dominik Troxler <dominik.troxler@gmx.ch>
2023-01-18 12:23:21 +0100

    changes

--- src/test/java/game/BoardTest.java
+++ src/test/java/game/BoardTest.java
@@ -1,5 +1,7 @@
 package game;
 
+import org.junit.Assert;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
@@ -13,13 +15,21 @@ class BoardTest {
 
     @org.junit.jupiter.api.Test
     void getBoard() {
+        assert (board.getBoard()==board);
     }
 
     @org.junit.jupiter.api.Test
     void getGrid() {
+        String[][] grid=new String[10][10];
+        Assert.assertArrayEquals(grid, board.getGrid());
     }
 
     @org.junit.jupiter.api.Test
     void setGrid() {
+        board.setGrid("A",new int[]{12,13});
+        String [][] grid=new String[10][10];
+        grid[1][2]="A";
+        grid[1][3]="A";
+        Assert.assertArrayEquals(grid, board.getGrid());
     }
 }
