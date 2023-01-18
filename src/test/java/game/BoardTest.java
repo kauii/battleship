2a608d2402daf88cad23935fc21e38f63f3f1550
Dominik Troxler <dominik.troxler@gmx.ch>
2023-01-18 12:02:04 +0100

    changes

---
+++ src/test/java/game/BoardTest.java
@@ -0,0 +1,25 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    Board board;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        board=new Board();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBoard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGrid() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setGrid() {
+    }
+}
